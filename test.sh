4f71e61a2ab60a3100f1be45744e40b8eeb5b14f
bitsofiot <139626590+bitsofiot@users.noreply.github.com>
2023-07-16 12:59:18 +0200

    Update test.sh

--- test.sh
+++ test.sh
@@ -1,12 +1,11 @@
 #!/bin/bash
-sudo -i
-sudo apt update
-sudo echo "$nrconf{restart} = 'a';" >> /etc/needrestart/needrestart.conf
-sudo apt install npm -y
-sudo npm install -g --unsafe-perm node-red
-sudo npm install -g pm2
-sudo pm2 start /usr/local/bin/node-red -- -v
-sudo pm2 save
-sudo pm2 startup
-sudo pm2 startup systemd
+apt update
+echo "$nrconf{restart} = 'a';" >> /etc/needrestart/needrestart.conf
+apt install npm -y
+npm install -g --unsafe-perm node-red
+npm install -g pm2
+pm2 start /usr/local/bin/node-red -- -v
+pm2 save
+pm2 startup
+pm2 startup systemd
 node-red
