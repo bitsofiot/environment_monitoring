30b71bd7064a4adecf39a7d4abc9945d3344eaaa
bitsofiot <139626590+bitsofiot@users.noreply.github.com>
2023-07-17 14:58:25 +0200

    Update test.sh

--- test.sh
+++ test.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
 sudo apt update
+sudo apt dist-upgrade -y
 sudo sed -i 's/#$nrconf{restart} = '"'"'i'"'"';/$nrconf{restart} = '"'"'a'"'"';/g' /etc/needrestart/needrestart.conf
 sudo sed -i "s/#\$nrconf{kernelhints} = -1;/\$nrconf{kernelhints} = -1;/g" /etc/needrestart/needrestart.conf
 
