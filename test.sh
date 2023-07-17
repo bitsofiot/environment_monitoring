e42999a5324e6989098ff1b9bc2a0780c3e0e0f0
bitsofiot <139626590+bitsofiot@users.noreply.github.com>
2023-07-17 12:50:46 +0200

    Update test.sh

--- test.sh
+++ test.sh
@@ -1,6 +1,8 @@
 #!/bin/bash
 sudo apt update
 sudo sed -i 's/#$nrconf{restart} = '"'"'i'"'"';/$nrconf{restart} = '"'"'a'"'"';/g' /etc/needrestart/needrestart.conf
+sudo sed -i "s/#\$nrconf{kernelhints} = -1;/\$nrconf{kernelhints} = -1;/g" /etc/needrestart/needrestart.conf
+
 sudo apt install npm -y
 sudo npm install -g --unsafe-perm node-red
 sudo npm install -g pm2
@@ -8,3 +10,7 @@ sudo pm2 start /usr/local/bin/node-red -- -v
 sudo pm2 save
 sudo pm2 startup
 sudo pm2 startup systemd
+
+
+sudo add-apt-repository ppa:mosquitto-dev/mosquitto-ppa -y
+sudo apt install mosquitto --assume-yes --allow
