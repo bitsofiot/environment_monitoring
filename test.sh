08e836b796e60f8d8584e08d0f096e3d34ed63e5
bitsofiot <139626590+bitsofiot@users.noreply.github.com>
2023-09-15 15:08:25 +0200

    Update test.sh

--- test.sh
+++ test.sh
@@ -1,17 +1,15 @@
 #!/bin/bash
-sudo su
-sudo apt update
-sudo apt dist-upgrade -y
-sudo sed -i 's/#$nrconf{restart} = '"'"'i'"'"';/$nrconf{restart} = '"'"'a'"'"';/g' /etc/needrestart/needrestart.conf
-sudo sed -i "s/#\$nrconf{kernelhints} = -1;/\$nrconf{kernelhints} = -1;/g" /etc/needrestart/needrestart.conf
-
-sudo apt install npm -y
-sudo npm install -g --unsafe-perm node-red
-sudo npm install -g pm2
-sudo pm2 start /usr/local/bin/node-red -- -v
-sudo pm2 save
-sudo pm2 startup
-sudo pm2 startup systemd
+apt update
+apt dist-upgrade -y
+sed -i 's/#$nrconf{restart} = '"'"'i'"'"';/$nrconf{restart} = '"'"'a'"'"';/g' /etc/needrestart/needrestart.conf
+sed -i "s/#\$nrconf{kernelhints} = -1;/\$nrconf{kernelhints} = -1;/g" /etc/needrestart/needrestart.conf
+apt install npm -y
+npm install -g --unsafe-perm node-red
+npm install -g pm2
+pm2 start /usr/local/bin/node-red -- -v
+pm2 save
+pm2 startup
+pm2 startup systemd
 
 cd ~/.node-red
 npm install node-red-dashboard
@@ -20,11 +18,11 @@ npm install node-red-dashboard
 npm install --no-audit --no-update-notifier --no-fund --save --save-prefix=~ --production --engine-strict node-red-dashboard@3.5.0
 
 
-sudo add-apt-repository ppa:mosquitto-dev/mosquitto-ppa -y
-sudo apt install mosquitto -y
+add-apt-repository ppa:mosquitto-dev/mosquitto-ppa -y
+apt install mosquitto -y
 
-echo "listener 1884 0.0.0.0" >> /etc/mosquitto/mosquitto.conf
-echo "allow_anonymous true" >> /etc/mosquitto/mosquitto.conf
+"listener 1884 0.0.0.0" >> /etc/mosquitto/mosquitto.conf
+"allow_anonymous true" >> /etc/mosquitto/mosquitto.conf
 
 wget https://raw.githubusercontent.com/bitsofiot/udemy/main/flows
 curl -X POST http://localhost:1880/flows -H 'content-type: application/json' -d @flows
