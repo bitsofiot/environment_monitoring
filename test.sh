3101dddb5e8d2620684556c5d1e325692663ffb5
bitsofiot <139626590+bitsofiot@users.noreply.github.com>
2023-09-17 20:03:49 +0200

    Update test.sh

--- test.sh
+++ test.sh
@@ -3,6 +3,19 @@ apt update
 apt dist-upgrade -y
 sed -i 's/#$nrconf{restart} = '"'"'i'"'"';/$nrconf{restart} = '"'"'a'"'"';/g' /etc/needrestart/needrestart.conf
 sed -i "s/#\$nrconf{kernelhints} = -1;/\$nrconf{kernelhints} = -1;/g" /etc/needrestart/needrestart.conf
+
+
+
+add-apt-repository ppa:mosquitto-dev/mosquitto-ppa -y
+apt install mosquitto -y
+
+chmod 755 /etc/mosquitto/
+
+echo "listener 1883 0.0.0.0" >> /etc/mosquitto/mosquitto.conf
+echo "allow_anonymous true" >> /etc/mosquitto/mosquitto.conf
+
+
+
 apt install npm -y
 npm install -g --unsafe-perm node-red
 npm install -g pm2
@@ -18,13 +31,6 @@ npm install node-red-dashboard
 npm install --no-audit --no-update-notifier --no-fund --save --save-prefix=~ --production --engine-strict node-red-dashboard@3.5.0
 
 
-add-apt-repository ppa:mosquitto-dev/mosquitto-ppa -y
-apt install mosquitto -y
-
-chmod 755 /etc/mosquitto/
-
-echo "listener 1884 0.0.0.0" >> /etc/mosquitto/mosquitto.conf
-echo "allow_anonymous true" >> /etc/mosquitto/mosquitto.conf
 
 #wget https://raw.githubusercontent.com/bitsofiot/udemy/main/flows
 #curl -X POST http://localhost:1880/flows -H 'content-type: application/json' -d @flows
